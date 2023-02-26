e2a2088322620e1244f152df3d4ada64a74054ab
Apexacme <apexacme@example.com>
2023-02-26 18:22:37 +0900

    Updated

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@ sudo apt-get update
 sudo apt-get install net-tools
 sudo apt install iputils-ping
 pip install httpie
+sudo apt install siege
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
